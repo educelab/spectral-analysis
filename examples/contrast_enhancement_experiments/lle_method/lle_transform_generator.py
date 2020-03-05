700691737cc402a27fb508c0f061c3aba441e24d
danieldopp <dbdo224@uky.edu>
2020-03-05 16:37:57 -0500

    started lle experiment code

--- examples/contrast_enhancement_experiments/lle_method/lle_transform_generator.py
+++ examples/contrast_enhancement_experiments/lle_method/lle_transform_generator.py
@@ -1,13 +1,13 @@
 import os
 
 if __name__ == '__main__':
-    DATA_DIR = "/data"
-    OUTPUT_DIR = "/outputs"
+    DATA_DIR = os.path.expandvars("$SCRATCH/2020-hyperspectral/data")
+    OUTPUT_DIR = os.path.expandvars("$SCRATCH/2020-hyperspectral/outputs")
     NUM_COMPONENTS = 5
     LLE_FILENAME = os.path.join(OUTPUT_DIR, "fitted_lle.joblib")
     DATA_MASK_REFERENCE_FILE = os.path.join(DATA_DIR) + "data_source.txt"
     FILE_TYPE = "tiff"
 
     print("Writing to a file in the outside directory")
-    with open(os.path.join(DATA_DIR, "new_file.txt")) as outfile:
+    with open(os.path.join(DATA_DIR, "new_file.txt"), 'w') as outfile:
         outfile.write("It worked!")
