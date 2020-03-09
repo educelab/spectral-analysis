96af2455d9ce5d0cde9ee11a843bac09d2ae71b0
danieldopp <dbdo224@uky.edu>
2020-03-09 16:54:40 -0400

    adjusted pca for the cluster

--- examples/contrast_enhancement_experiments/pca_method/pca_transform_generator.py
+++ examples/contrast_enhancement_experiments/pca_method/pca_transform_generator.py
@@ -19,11 +19,15 @@ def get_training_data_paths(f_name):
 
 
 if __name__ == '__main__':
-    NUM_COMPONENTS = 10
-    PROJECT_DIR = "../test_runs/PCA_test_masked_characters_floats/"
-    PCA_FILENAME = PROJECT_DIR + "fitted_pca.joblib"
+    print("Starting PCA Experiment", flush=True)
+
+    OUTPUT_DIR = os.path.expandvars("$SCRATCH/2020-hyperspectral/outputs/autoencoder_experiment")
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+    EMBEDDING_DIM = 3
+    PCA_FILENAME = os.path.join(OUTPUT_DIR, "fitted_pca.joblib")
     METHOD = "PCA"
-    DATA_MASK_REFERENCE_FILE = PROJECT_DIR + "data_source.txt"
+    DATA_REFERENCE = "/spectral-analysis/examples/contrast_enhancement_experiments/autoencoder_method/data_and_mask_paths.txt"
     FILE_TYPE = "tiff"
     # TODO, make the number of y slices pulled each time adaptive to ingest more data at once
     # TODO, figure out this banal multiprocessing for training and pulling data concurrently
@@ -34,17 +38,17 @@ if __name__ == '__main__':
         pca = load(PCA_FILENAME)
     else:
         if METHOD == "IterativePCA":
-            pca = IncrementalPCA(n_components=NUM_COMPONENTS)
+            pca = IncrementalPCA(n_components=EMBEDDING_DIM, whiten=True)
 
         elif METHOD == "PCA":
-            pca = PCA(n_components=NUM_COMPONENTS)
+            pca = PCA(n_components=EMBEDDING_DIM, whiten=True)
 
         else:
             raise ValueError(f"{METHOD} is an invalid PCA flavour")
 
         training_chunk = []
         num_examples = 0
-        for data_file, mask_file in get_training_data_paths(DATA_MASK_REFERENCE_FILE):
+        for data_file, mask_file in get_training_data_paths(DATA_REFERENCE):
             data_handler = SpectralDataHandler(data_file)
             x_max = data_handler.io.metadata["samples"]
             y_max = data_handler.io.metadata["lines"]
@@ -71,27 +75,32 @@ if __name__ == '__main__':
                         training_chunk = []
                         num_examples = 0
 
+        print("Done loading data", flush=True)
         if METHOD == "PCA":
             pca.fit(np.concatenate(training_chunk))
-            print(f"Trained on {num_examples} number of pixels")
+            print(f"Trained on {num_examples} number of pixels", flush=True)
 
         dump(pca, PCA_FILENAME)
 
-    for data_file, _ in get_training_data_paths(DATA_MASK_REFERENCE_FILE):
+    for data_file, _ in get_training_data_paths(DATA_REFERENCE):
+        print(f"Applying transform to {data_file}", flush=True)
         data_handler = SpectralDataHandler(data_file)
         x_max = data_handler.io.metadata["samples"]
         y_max = data_handler.io.metadata["lines"]
         z_max = data_handler.io.metadata["bands"]
 
-        image = np.zeros((y_max, x_max, NUM_COMPONENTS))
+        image = np.zeros((y_max, x_max, EMBEDDING_DIM))
 
-        for i in tqdm(range(y_max)):
+        for i in range(y_max):
             fitting_data = data_handler.io.get_volume_chunk((0, x_max), (i, i + 1), (0, z_max)).reshape((-1, z_max))
             image[i] = pca.transform(fitting_data)
 
-        for i in range(NUM_COMPONENTS):
+        for i in range(EMBEDDING_DIM):
             file_name = data_file.split("/")[-1].split(".")[0]
-            print(f"PCA results: Explained variance of component {i + 1}: {pca.explained_variance_ratio_[i]}")
-            imageio.imwrite(f"{PROJECT_DIR}{file_name}_component{i}.{FILE_TYPE}", image[:, :, i])
+            imageio.imwrite(os.path.join(OUTPUT_DIR, f"{file_name}_dimension{i}.{FILE_TYPE}"), image[:, :, i])
+
+        if EMBEDDING_DIM == 3:
+            file_name = data_file.split("/")[-1].split(".")[0]
+            imageio.imwrite(os.path.join(OUTPUT_DIR, f"{file_name}_false_color.{FILE_TYPE}"), image)
 
     print("Done")
